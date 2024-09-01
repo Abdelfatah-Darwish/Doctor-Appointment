555e660010a80a7175cf4be4ab1aef59f85b3e8b
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-09-01 15:42:31 +0300

    create textStyles

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ColorsManager {
@@ -11,3 +10,7 @@ class ColorsManager {
   static const Color moreLightGray = Color(0xFFFDFDFF);
   static const Color moreLighterGray = Color(0xFFF5F5F5);
 }
+
+// static: This keyword means the variable belongs to the class itself rather than to any specific instance of the class. This allows you to access mainBlue directly through the class without needing to create an object of the class.
+
+// const: This keyword indicates that the value of mainBlue is constant and will not change after being initialized. In Dart, const variables must be set at compile-time and are immutable.
